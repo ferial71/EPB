0f503eecc5b5a3f2c1d7b8314538b1f5a4310ae1
ferial71 <ff_bouzoul@esi.dz>
2020-06-10 18:30:13 +0100

    added crf token to profile blade's header

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <title>AdminLTE 3 | User Profile</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
