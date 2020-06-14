9a8c9555a22bffb5c948cc1d967986e01a77913f
ferial71 <ff_bouzoul@esi.dz>
2020-06-14 15:31:32 +0100

    added a notification component and methods

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('notification', require('./components/Notification.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +29,17 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    data: {
+        notifications: ''
+    },
+    created() {
+        axios.post('/notification/get').then(response => {
+            this.notifications = response.data;
+        });
+
+        var userId = $('meta[name="userId"]').attr('content');
+        Echo.private('App.User.' + userId).notification((notification) => {
+            this.notifications.push(notification);
+        });
+    }
 });
