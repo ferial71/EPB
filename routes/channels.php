0bac2fc21c907b3f6395cf9119bd90ee0c1f1861
ferial71 <ff_bouzoul@esi.dz>
2020-07-14 14:06:49 +0100

    set the condition to private channel to true

--- routes/channels.php
+++ routes/channels.php
@@ -14,7 +14,7 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return true ;
 });
 //Broadcast::channel('users.{id}', function ($user, $id) {
 //
