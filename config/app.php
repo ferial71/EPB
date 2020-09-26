a1073502db89eb2ff222703ff3382fe6fa52f096
ferial71 <ff_bouzoul@esi.dz>
2020-09-26 23:00:46 +0100

    removed etl service provider

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Marquine\Etl\EtlServiceProvider::class,
+        //Marquine\Etl\EtlServiceProvider::class,
+        Monarobase\CountryList\CountryListServiceProvider::class,
 
     ],
 
@@ -232,6 +233,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Countries' => Monarobase\CountryList\CountryListFacade::class,
 
     ],
 
