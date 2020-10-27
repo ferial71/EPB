115006f730f19e09238cec8f1516d8d388e52025
ferial71 <ff_bouzoul@esi.dz>
2020-10-27 19:28:44 +0100

    created cpn controller

---
+++ app/Http/Controllers/CpnController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CPN;
+use App\emplacement;
+use App\escale;
+use App\Formulaire;
+use App\navire;
+use Illuminate\Http\Request;
+
+class CpnController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+//        $emplacements = emplacement::all();
+//        $escales= escale::all();
+//        $navire_id = [];
+////dd($navire_id);
+//        $navires = navire::where('id',$navire_id);
+        $cpns= Cpn::all();
+
+        return view('cpns.index', compact('cpns'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
